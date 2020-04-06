7b85d48bfd717ede3b9230ed554ac67cc510165a
Alessandro Ampala <alessandro.ampala@edu.unito.it>
2020-04-07 00:12:12 +0200

    Terminated application development Minor changes: - Full working command line argument parsing - Now date and time are printed when an algorithm is executed - All the alloc'd memory is now freed at the end of the program

--- Es1/app/sorting_app.c
+++ Es1/app/sorting_app.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 #include "array.h"
 #include "sorting.h"
 
@@ -17,51 +18,14 @@ typedef struct RecordArray{
   int length;
 } RecordArray;
 
-RecordArray load_data(char const* filename)
+int compare_field1(Record* obj1, Record* obj2)
 {
-  FILE* file = fopen(filename, "r");
+  return strcmp(obj1->field1, obj2->field1);
+}
 
-  #define MAX_BUF_LEN 1024
-  int id;
-  char field1[MAX_BUF_LEN];
-  int field2;
-  float field3;
-
-  int size = 100;
-  Record* records = malloc(sizeof(Record) * size);
-
-  int lineno = 0;
-  while(!feof(file))
-  {
-
-    int n = fscanf(file, "%d,%1024[^,],%d,%f\n", &id, field1, &field2, &field3);
-    lineno++;
-
-    if(n != 4)
-    {
-        if(feof(file))
-            continue;
-        printf("Error while reading file at line: %d\n", lineno);
-        exit(EXIT_FAILURE);
-    }
-
-    if(lineno == size)
-    {
-      size *= 2;
-      records = realloc(records, sizeof(Record) * size);
-    }
-
-    records[lineno - 1].id = id;
-    records[lineno - 1].field1 = strndup(field1, MAX_BUF_LEN);
-    records[lineno - 1].field2 = field2;
-    records[lineno - 1].field3 = field3;
-  }
-
-  RecordArray ra;
-  ra.array = records;
-  ra.length = lineno;
-
-  return ra;
+int compare_field2(Record* obj1, Record* obj2)
+{
+  return obj1->field2 - obj2->field2;
 }
 
 int compare_field3(Record* obj1, Record* obj2)
@@ -74,23 +38,20 @@ int compare_field3(Record* obj1, Record* obj2)
     return -1;
 }
 
-int compare_field2(Record* obj1, Record* obj2)
-{
-  return obj1->field2 - obj2->field2;
-}
-
-int compare_field1(Record* obj1, Record* obj2)
-{
-  return strcmp(obj1->field1, obj2->field1);
-}
-
 void print_usage()
 {
-  fprintf(stderr, "Unrecognized option\n");
+    printf("\nUSAGE:\n");
+    printf("sorting_app < -i | -q > < -1 | -2 | -3 > <filename>\n");
+    printf("    -i: sort using Insertion sort algorithm\n");
+    printf("    -q: sort using Quicksort algorithm\n");
+    printf("    -1: sort according to the first field ascending order\n");
+    printf("    -2: sort according to the second field ascending order\n");
+    printf("    -3: sort according to the third field ascending order\n");
 }
 
 void wrong_options()
 {
+  fprintf(stderr, "Unrecognized option\n");
   print_usage();
   exit(EXIT_FAILURE);
 }
@@ -101,25 +62,131 @@ void get_options(int argc, char* const *argv, char* algorithm, int* field)
   {
     case 'q':
     case 'i':
-    printf("%c\n", *algorithm);
       break;
     case '?':
-      fprintf(stderr, "Unrecognized option\n");
+    case -1:
       wrong_options();
       break;
   }
 
-  switch(*field = getopt(argc, argv, "123") - '0')
+  switch(*field = getopt(argc, argv, "123"))
   {
-    case 1:
-    case 2:
-    case 3:
-    printf("%d\n", *field);
+    case '1':
+    case '2':
+    case '3':
+      *field -= '0';
       break;
     case '?':
-      fprintf(stderr, "Unrecognized option\n");
+    case -1:
       wrong_options();
       break;
   }
 }
+
+RecordArray load_data(char const* filename)
+{
+  FILE* file = fopen(filename, "r");
+  if(file == NULL)
+  {
+    fprintf(stderr, "Error while opening file, terminating\n");
+    print_usage();
+    exit(EXIT_FAILURE);
+  }
+
+  #define MAX_BUF_LEN 1024
+  int id;
+  char field1[MAX_BUF_LEN];
+  int field2;
+  float field3;
+  int size = 100;
+  int lineno = 0;
+  Record* records = malloc(sizeof(Record) * size);
+
+  while(!feof(file))
+  {
+    int n = fscanf(file, "%d,%1024[^,],%d,%f\n", &id, field1, &field2, &field3);
+    lineno++;
+
+    if(n != 4)
+    {
+        if(feof(file))
+            continue;
+        printf("Error while reading file at line: %d\n", lineno);
+        exit(EXIT_FAILURE);
+    }
+    if(lineno == size)
+    {
+      size *= 2;
+      records = realloc(records, sizeof(Record) * size);
+    }
+
+    records[lineno - 1].id = id;
+    records[lineno - 1].field1 = strndup(field1, MAX_BUF_LEN);
+    records[lineno - 1].field2 = field2;
+    records[lineno - 1].field3 = field3;
+  }
+
+  fclose(file);
+
+  RecordArray ra;
+  ra.array = records;
+  ra.length = lineno;
+  return ra;
+}
+
+void free_elements(RecordArray* a)
+{
+  for(int i = 0; i < a->length; i++)
+  {
+    free(a->array[i].field1);
+    free(&(a->array[i]));
+  }
+}
+
+void update_time(time_t* rawtime, struct tm ** timeinfo)
+{
+  time(rawtime);
+  *timeinfo = localtime(rawtime);
+}
+
+int main(int argc, char* const *argv)
+{
+  char algorithm;
+  int field;
+  RecordArray ra;
+  compare_fun compare[] = {(compare_fun) compare_field1, (compare_fun) compare_field2, (compare_fun) compare_field3};
+
+  get_options(argc, argv, &algorithm, &field);
+  
+  if(optind < argc)
+  {
+    printf("Loading file...\n");
+    ra = load_data(argv[optind]); //LOAD FILE
+  }
+  else
+    wrong_options();
+
+  Array* a = Array_new(ra.array, ra.length, sizeof(Record));
+
+  time_t rawtime;
+  struct tm * timeinfo;
+  
+  update_time(&rawtime, &timeinfo);
+  switch(algorithm)
+  {
+    case 'q':
+      printf("Starting Quicksort on field%d\t%s", field, asctime(timeinfo));
+      quick_sort(a, compare[field - 1]);
+      break;
+    case 'i':
+      printf("Starting Insertion sort on field%d\t%s", field, asctime(timeinfo));
+      insertion_sort(a, compare[field - 1]);
+      break;
+  }
+
+  update_time(&rawtime, &timeinfo);
+  printf("Successfully terminated\t\t%s", asctime(timeinfo));
+
+  free_elements(&ra);
+  Array_free(a);
 }
