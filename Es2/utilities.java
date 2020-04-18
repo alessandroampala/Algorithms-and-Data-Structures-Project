51c0c43c8e77cfdd27b192c68ee992b41483f5ef
Mattia Borrelli <SbX?1398>
2020-04-18 15:54:38 +0200

    ES2: add the core function for DY version of edit_distance

--- Es2/utilities.java
+++ Es2/utilities.java
@@ -1,5 +1,14 @@
 public class utilities{
 
+	public static int min(int obj1, int obj2, int obj3){
+		int m = obj1;
+		if(m > obj2)
+			m = obj2;
+		if(m > obj3)
+			m = obj3;
+		return m;
+	}
+
 	public static int edit_distace(String s1, String s2, int value, int type){
 		int d_nope = 0, d_canc = 0, d_ins = 0;
 		
@@ -22,12 +31,30 @@ public class utilities{
 		return min(d_nope, d_canc, d_ins);
 	}
 
-	public static int min(int obj1, int obj2, int obj3){
-		int m = obj1;
-		if(m > obj2)
-			m = obj2;
-		if(m > obj3)
-			m = obj3;
-		return m;
+	public static int dy_ed(String s1, String s2, int[][]edit_tab, int[][] canc_lin, int[][] ins_lin){
+		int d_nope = 0, d_canc = 0, d_ins = 0;
+		if(s1.length() == 0)
+			return s2.length();
+		else if(s2.length() == 0)
+			return s1.length();
+		else{
+			if(edit_tab[s1.length()][s2.length()] == 0)
+			{
+				if(s1.charAt(0) == s2.charAt(0) )
+					edit_tab[s1.length()][s2.length()] = dy_ed(s1.substring(1), s2.substring(1), edit_tab, canc_lin, ins_lin);
+				else
+				{
+					edit_tab[s1.length()][s2.length()] = Integer.MAX_VALUE;
+					
+					if(canc_lin[s2.length()][s2.length()] == 0)
+						canc_lin[s2.length()][s2.length()] = 1 + dy_ed(s1, s2.substring(1), edit_tab, canc_lin, ins_lin);
+					if(ins_lin[s1.length()][s2.length()] == 0)
+						ins_lin[s1.length()][s2.length()] = 1 + dy_ed(s1.substring(1), s2, edit_tab, canc_lin, ins_lin);	
+				}
+		}
+
+		return min(edit_tab[s1.length()][s2.length()], canc_lin[s2.length()][s2.length()], ins_lin[s1.length()][s2.length()]);
+		}
 	}
+
 }
