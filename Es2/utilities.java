7d389880359a4e5ec9cf168a31ad948b91c7b3a3
Mattia Borrelli <SbX?1398>
2020-04-18 16:19:21 +0200

    ES2: comments and less adds to utilities file (completed)

--- Es2/utilities.java
+++ Es2/utilities.java
@@ -1,5 +1,8 @@
 public class utilities{
-
+	
+	/*
+	 * return the minimum number between three numbers.
+	 */
 	public static int min(int obj1, int obj2, int obj3){
 		int m = obj1;
 		if(m > obj2)
@@ -9,7 +12,10 @@ public class utilities{
 		return m;
 	}
 
-	public static int edit_distace(String s1, String s2, int value, int type){
+	/*
+	 * return the edit distance beetwen two strings
+	 */
+	public static int edit_distace(String s1, String s2){
 		int d_nope = 0, d_canc = 0, d_ins = 0;
 		
 		if(s1.length() == 0)
@@ -19,41 +25,60 @@ public class utilities{
 		else{
 			//d_nope case
 			if(s1.charAt(0) == s2.charAt(0))
-				d_nope = edit_distace(s1.substring(1), s2.substring(1), value + 1, 1);
+				d_nope = edit_distace(s1.substring(1), s2.substring(1));
 			else
 					d_nope = Integer.MAX_VALUE;
 			//d_canc
-			d_canc = 1 + edit_distace(s1, s2.substring(1), value + 1, 2);
+			d_canc = 1 + edit_distace(s1, s2.substring(1));
 			//d_ins
-			d_ins = 1 + edit_distace(s1.substring(1), s2,  value + 1, 3);	
+			d_ins = 1 + edit_distace(s1.substring(1), s2);	
 		}
 
 		return min(d_nope, d_canc, d_ins);
 	}
 
-	public static int dy_ed(String s1, String s2, int[][]edit_tab, int[][] canc_lin, int[][] ins_lin){
+	/*
+	 * Return the edit distance between two strings.
+	 * This method prepares data structures for the dynamic version of
+	 * edit_distance's method. It uses three matrices to make the
+	 * memoization technique in dy_ed possible.
+	 */
+	public static int dy_edit_distace(String s1, String s2){
+		int[][] nope_tab = new int[s1.length() + 1][s2.length() + 1];
+		int[][] canc_tab = new int[s2.length() + 1][s2.length() + 1];
+		int[][] ins_tab = new int[s1.length() + 1][s2.length() + 1];		
+		return dy_ed(s1, s2, nope_tab, canc_tab, ins_tab);
+	}
+
+	/*
+	 * Calculate the edit distance between two strings using the memoization. Before
+	 * each recursive call, the method checks if the value has already been calculated in
+	 * a previous call and, eventually, return immediately this value as result. This process
+	 * make the algorithm more efficient avoiding repetitive recursive calls.
+	 */
+	public static int dy_ed(String s1, String s2, int[][] nope_tab, int[][] canc_tab, int[][] ins_tab){
 		int d_nope = 0, d_canc = 0, d_ins = 0;
 		if(s1.length() == 0)
 			return s2.length();
 		else if(s2.length() == 0)
 			return s1.length();
 		else{
-			if(edit_tab[s1.length()][s2.length()] == 0)
+			if(nope_tab[s1.length()][s2.length()] == 0)
 			{
 				if(s1.charAt(0) == s2.charAt(0) )
-					edit_tab[s1.length()][s2.length()] = dy_ed(s1.substring(1), s2.substring(1), edit_tab, canc_lin, ins_lin);
+					nope_tab[s1.length()][s2.length()] = dy_ed(s1.substring(1), s2.substring(1), nope_tab, canc_tab, ins_tab);
 				else
 				{
-					edit_tab[s1.length()][s2.length()] = Integer.MAX_VALUE;
+					nope_tab[s1.length()][s2.length()] = Integer.MAX_VALUE;
 					
-					if(canc_lin[s2.length()][s2.length()] == 0)
-						canc_lin[s2.length()][s2.length()] = 1 + dy_ed(s1, s2.substring(1), edit_tab, canc_lin, ins_lin);
-					if(ins_lin[s1.length()][s2.length()] == 0)
-						ins_lin[s1.length()][s2.length()] = 1 + dy_ed(s1.substring(1), s2, edit_tab, canc_lin, ins_lin);	
+					if(canc_tab[s2.length()][s2.length()] == 0)
+						canc_tab[s2.length()][s2.length()] = 1 + dy_ed(s1, s2.substring(1), nope_tab, canc_tab, ins_tab);
+					if(ins_tab[s1.length()][s2.length()] == 0)
+						ins_tab[s1.length()][s2.length()] = 1 + dy_ed(s1.substring(1), s2, nope_tab, canc_tab, ins_tab);	
 				}
 		}
 
-		return min(edit_tab[s1.length()][s2.length()], canc_lin[s2.length()][s2.length()], ins_lin[s1.length()][s2.length()]);
+		return min(nope_tab[s1.length()][s2.length()], canc_tab[s2.length()][s2.length()], ins_tab[s1.length()][s2.length()]);
 		}
 	}
 
