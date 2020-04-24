1e44a2764cdcdf88071799f94ef9544d5a466b39
Mattia Borrelli <SbX?1398>
2020-04-24 19:22:46 +0200

    [Es2] test file completed

--- Es2/Es2Test.java
+++ Es2/Es2Test.java
@@ -1,4 +1,5 @@
-/* COMPILING: 
+/**
+ * COMPILING: 
  * compile with javac -cp .;junit-4.13.jar;hamcrest-core-1.3.jar ED_test.java
  *
  * RUNNING:
@@ -28,7 +29,9 @@ public class EdTest{
 		{
 			case 0: return "";
 			case 1: return "a";
-			default: return "ciao";
+			default: 
+			System.out.println("Error: Invalid argument in createBaseCase");
+			System.exit(0);
 		}
 	}
 
@@ -36,8 +39,16 @@ public class EdTest{
 	{
 		switch(n)
 		{
-			case 1: return "Tartaruga";
-			case 2: return "trotterellando";
+			case 0: return "Ciao";
+			case 1: return "magone";
+			case 2: return "arcimago";
+			case 3: return "testo";
+			case 4: return "vinai";
+			case 5: return "Tartaruga";
+			case 6: return "trotterellando";
+			default: 
+			System.out.println("Error: Invalid argument in createNormalCase");
+			System.exit(0);
 		}
 	}
 
@@ -58,47 +69,116 @@ public class EdTest{
 	public static void baseEd()
 	{
 		int result = -1;
-		String a, b;
+		String s1, s2;
 
 		// Length 0 case
-		a = createBaseCase(0);
-		b = createBaseCase(0);
-		result = editDistance(a,b);
+		s1 = createBaseCase(0);
+		s2 = createBaseCase(0);
+		result = Es2Library.editDistance(s1,s2);
 		assertEquals(0, result);
 
 		// Length 0 for string a
-		a = createBaseCase(0);
-		b = createBaseCase(1);
-		result = editDistance(a, b);
+		s1 = createBaseCase(0);
+		s2 = createBaseCase(1);
+		result = Es2Library.editDistance(s1, s2);
 		assertEquals(1, result);
 
 		// length 0 for string b
-		a = createBaseCase(1);
-		b = createBaseCase(0);
-		result = editDistance(a, b);
+		s1 = createBaseCase(1);
+		s2 = createBaseCase(0);
+		result = Es2Library.editDistance(s1, s2);
 		assertEquals(1, result);
-
-		// a equals b
-		a = createBaseCase(2);
-		b = createBaseCase(2);
-		result = editDistance(a,b);
-		assertEquals(0, result);
+	
 	}
 
 	@Test
 	public static void normalCaseEd()
-	{
+	{			
+		int result = -1;
+		String s1, s2;
+
+		// a equals b
+		s1 = createNormalCase(0);
+		s2 = createNormalCase(0);
+		result = Es2Library.editDistanceDyn(s1,s2);
+		assertEquals(0, result);
+
+		// substring case
+		s1 = createNormalCase(1);
+		s2 = createNormalCase(2);
+		result = Es2Library.editDistanceDyn(s1,s2);
+		assertEquals(6,result);
+
+		// all different letters
+		s1 = createNormalCase(3);
+		s2 = createNormalCase(4);
+		result = Es2Library.editDistanceDyn(s1,s2);
+		assertEquals(10,result);
+
+		// normal case
+		s1 = createNormalCase(5);
+		s2 = createNormalCase(6);
+		result = Es2Library.editDistanceDyn(s1, s2);
+		assertEquals(15,result);
+
 
 	}
 
 	@Test
 	public static void baseCaseEdDyn()
 	{
+		int result = -1;
+		String s1, s2;
 
+		// Length 0 case
+		s1 = createBaseCase(0);
+		s2 = createBaseCase(0);
+		result = Es2Library.editDistanceDyn(s1,s2);
+		assertEquals(0, result);
+
+		// Length 0 for string a
+		s1 = createBaseCase(0);
+		s2 = createBaseCase(1);
+		result = Es2Library.editDistanceDyn(s1, s2);
+		assertEquals(1, result);
+
+		// length 0 for string b
+		s1 = createBaseCase(1);
+		s2 = createBaseCase(0);
+		result = Es2Library.editDistanceDyn(s1, s2);
+		assertEquals(1, result);
+
+		
 	}
 
 	public static void normalCaseEdDyn()
 	{
+		int result = -1;
+		String s1, s2;
+
+		// a equals b
+		s1 = createNormalCase(0);
+		s2 = createNormalCase(0);
+		result = Es2Library.editDistanceDyn(s1,s2);
+		assertEquals(0, result);
+
+		// substring case
+		s1 = createNormalCase(1);
+		s2 = createNormalCase(2);
+		result = Es2Library.editDistanceDyn(s1,s2);
+		assertEquals(6,result);
+
+		// all different letters
+		s1 = createNormalCase(3);
+		s2 = createNormalCase(4);
+		result = Es2Library.editDistanceDyn(s1,s2);
+		assertEquals(10,result);
+
+		// normal case
+		s1 = createNormalCase(5);
+		s2 = createNormalCase(6);
+		result = Es2Library.editDistanceDyn(s1, s2);
+		assertEquals(15,result);
 
 	}
 
