cc9ddbee13f29c918b19182911c9df314ccd8dd0
Mattia Borrelli <SbX?1398>
2020-04-25 16:10:05 +0200

    [Es2] Dynamic version works perfectly now ~ Test passed at all

--- Es2/Es2Library.java
+++ Es2/Es2Library.java
@@ -3,7 +3,7 @@ public class Es2Library{
 	/*
 	 * return the minimum number between three numbers.
 	 */
-	private static int min(int obj1, int obj2, int obj3){
+	public static int min(int obj1, int obj2, int obj3){
 		int m = obj1;
 		if(m > obj2)
 			m = obj2;
@@ -26,7 +26,7 @@ public class Es2Library{
 			if(s1.charAt(0) == s2.charAt(0))
 				dNope = editDistance(s1.substring(1), s2.substring(1));
 			else
-					dNope = Integer.MAX_VALUE;
+				dNope = Integer.MAX_VALUE;
 			dCanc = 1 + editDistance(s1,s2.substring(1));
 			dIns = 1 + editDistance(s1.substring(1), s2);	
 		}
@@ -42,7 +42,7 @@ public class Es2Library{
 	 */
 	public static int editDistanceDyn(String s1, String s2){
 		int[][] nopeTab = new int[s1.length() + 1][s2.length() + 1];
-		int[][] cancTab = new int[s2.length() + 1][s2.length() + 1];
+		int[][] cancTab = new int[s1.length() + 1][s2.length() + 1];
 		int[][] insTab = new int[s1.length() + 1][s2.length() + 1];		
 		return dyEd(s1, s2, nopeTab, cancTab, insTab);
 	}
@@ -53,29 +53,24 @@ public class Es2Library{
 	 * a previous call and, eventually, return immediately this value as result. This process
 	 * make the algorithm more efficient avoiding repetitive recursive calls.
 	 */
-	private static int dyEd(String s1, String s2, int[][] nopeTab, int[][] cancTab, int[][] insTab){
+	public static int dyEd(String s1, String s2, int[][] nopeTab, int[][] cancTab, int[][] insTab){
 		if(s1.length() == 0)
 			return s2.length();
 		else if(s2.length() == 0)
 			return s1.length();
 		else{
-			if(nopeTab[s1.length()][s2.length()] == 0)
-			{
-				if(s1.charAt(0) == s2.charAt(0) )
+			if((nopeTab[s1.length()][s2.length()] == 0)){
+				if(s1.charAt(0) == s2.charAt(0))
 					nopeTab[s1.length()][s2.length()] = dyEd(s1.substring(1), s2.substring(1), nopeTab, cancTab, insTab);
 				else
-				{
 					nopeTab[s1.length()][s2.length()] = Integer.MAX_VALUE;
-					
-					if(cancTab[s2.length()][s2.length()] == 0)
-						cancTab[s2.length()][s2.length()] = 1 + dyEd(s1, s2.substring(1), nopeTab, cancTab, insTab);
-					if(insTab[s1.length()][s2.length()] == 0)
-						insTab[s1.length()][s2.length()] = 1 + dyEd(s1.substring(1), s2, nopeTab, cancTab, insTab);	
-				}
+			}
+			if(cancTab[s1.length()][s2.length()] == 0)
+				cancTab[s1.length()][s2.length()] = 1 + dyEd(s1, s2.substring(1), nopeTab, cancTab, insTab);
+			if(insTab[s1.length()][s2.length()] == 0)
+				insTab[s1.length()][s2.length()] = 1 + dyEd(s1.substring(1), s2, nopeTab, cancTab, insTab);	
 		}
-
-		return min(nopeTab[s1.length()][s2.length()], cancTab[s2.length()][s2.length()], insTab[s1.length()][s2.length()]);
-		}
+		return min(nopeTab[s1.length()][s2.length()], cancTab[s1.length()][s2.length()], insTab[s1.length()][s2.length()]);
 	}
 
 }
